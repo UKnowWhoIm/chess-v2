ddd5f16875c6c23238dd6d5d6d5eaaefef44435e
Sidharth <sidforjee@gmail.com>
2021-02-05 01:22:11 +0530

    restructured folders

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ var path = require("path");
 var app = express();
 var server = http.createServer(app);
 const io = require('socket.io')(server);
-const db = require("./dbCrud");
 server.listen(3000);
  
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/test', (req, res) => {
-    res.sendFile(__dirname + '/testSocket.html');
-});
-
-app.use('/static', express.static(path.join(__dirname, '/public')));
-//app.use('/socket.io', express.static(path.join(__dirname, '/node_modules/socket.io/client-dist')));
+app.use('/static', express.static(path.join(__dirname, '/static')));
 
 io.on("connection", socket => socketEvents(io, socket));
